2e6dfc6811e177639de4fd06e93f3d28cca21cd8
SikuAudio <sikuaudio@gmail.com>
2022-07-12 17:36:17 -0500

    Upload Hue Trails

---
+++ projects/Hue Trails/sketch.js
@@ -0,0 +1,62 @@
+let x, y, xoff, yoff, r, grow, h, s, b
+
+function setup () {
+  colorMode(HSB)
+  rectMode(CENTER)
+  createCanvas(windowWidth, windowHeight)
+  background(0)
+
+  x = width / 2
+  y = height / 2
+  r = 0
+
+  xoff = 0
+  yoff = 0
+
+  grow = true
+
+  h = 0
+  s = 0
+  b = 100
+
+  hueDir = true
+}
+
+function draw () {
+  fill(100)
+
+  x = noise(xoff) * width
+  y = noise(yoff) * height
+
+  xoff += 0.005
+  yoff += 0.004
+
+  if (r > 200) {
+    grow = false
+  } else if (r < 5) {
+    grow = true
+  }
+
+  if (grow) {
+    r += 0.1
+    constrain((s += 0.1), 0, 100)
+  } else {
+    r -= 0.1
+    constrain((s -= 0.1), 20, 100)
+  }
+
+  if (h < 0) {
+    hueDir = true
+  } else if (h > 360) {
+    hueDir = false
+  }
+  if (hueDir) {
+    constrain((h += 0.1), 0, 360)
+  } else {
+    constrain((h -= 0.1), 0, 360)
+  }
+
+  stroke(0, 0.6)
+  fill(h, s, b)
+  circle(x, y, r)
+}
